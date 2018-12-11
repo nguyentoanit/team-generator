484cb5dafde5654fe3518561048cf8ffd02e89a3
toan_nv <nguyentoandev@gmail.com>
2018-12-11 17:03:35 +0700

    Convert to VueJS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,155 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>Team Generator</h1>
+    <form>
+      <!-- <div class="form-group">
+        <label for="file">Members CSV file:</label>
+        <input class="form-control" id="file" type="file" ref="members" @change="upload">
+      </div>-->
+      <div class="form-group">
+        <label for="file">Teams:</label>
+        <input class="form-control" id="teams" type="number" name="teams" min="2" value="2">
+      </div>
+      <div class="form-group">
+        <label for="members">Members:</label>
+        <textarea class="form-control" v-model="content"></textarea>
+        <br>
+        <div class="form-group">
+          <input type="button" class="btn btn-primary" value="Generate" @click="generate">
+        </div>
+      </div>
+    </form>
+    <br>
+    <div>Result</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      file: [],
+      content: "K ,1987\nA ,1978",
+      members: [],
+      teams: [
+        {
+          totalAge: 0,
+          member: []
+        },
+        {
+          totalAge: 0,
+          member: []
+        },
+        {
+          totalAge: 0,
+          member: []
+        }
+      ]
+    };
+  },
+  methods: {
+    getTeam: function() {
+      var index = 0;
+      for (var i = 0; i < this.teams.length; i++) {
+        // Get size of team
+        var objectMemberLength = this.teams[index].member.length;
+        var objectSum = this.teams[index].totalAge;
+        var currentMemberLength = this.teams[i].member.length;
+        var currentSum = this.teams[i].totalAge;
+
+        // Choose team
+        if (currentMemberLength == 0) {
+          index = i;
+          break;
+        } else if (currentMemberLength < objectMemberLength) {
+          index = i;
+        } else if (
+          currentMemberLength == objectMemberLength &&
+          currentSum > objectSum
+        ) {
+          index = i;
+        }
+      }
+      return index;
+    },
+    generate: function() {
+      var lines = this.content.split("\n");
+      for (var line = 0; line < lines.length; line++) {
+        var lineContent = lines[line].split(",");
+        var member = {
+          name: lineContent[0].trim(),
+          year: parseInt(lineContent[1].trim())
+        };
+        // Push member to array
+        this.members.push(member);
+      }
+      // Sort member by birthYear
+      this.members = _.sortBy(this.members, [
+        function(o) {
+          return o.year;
+        }
+      ]);
+      // Add members to teams
+      for (var i = 0; i < this.members.length; i++) {
+        var index = this.getTeam();
+        this.teams[index].totalAge += this.members[i].year;
+        this.teams[index].member.push(this.members[i]);
+      }
+      console.log(this.teams);
     }
+    // upload: function(ev) {
+    //   var file = ev.target.files[0];
+    //   var reader = new FileReader();
+    //   reader.onload = function(progressEvent) {
+    //     // By lines
+    //     var members = [];
+    //     var lines = this.result.split("\n");
+    //     for (var line = 0; line < lines.length; line++) {
+    //       var lineContent = lines[line].split(",");
+    //       var member = {
+    //         name: lineContent[0].trim(),
+    //         year: parseInt(lineContent[1].trim())
+    //       };
+    //       // Push member to array
+    //       members.push(member);
+    //     }
+
+    //     // Sort member by birthYear
+    //     members = _.sortBy(members, [
+    //       function(o) {
+    //         return o.year;
+    //       }
+    //     ]);
+
+    //     // Inital Teams
+    //     var teams = [
+    //       {
+    //         totalAge: 0,
+    //         member: []
+    //       },
+    //       {
+    //         totalAge: 0,
+    //         member: []
+    //       },
+    //       {
+    //         totalAge: 0,
+    //         member: []
+    //       }
+    //     ];
+    //     var index = this.getTeam(teams);
+
+    //     // Add members to teams
+    //     members.forEach(function(item, index) {
+    //       teams[index].totalAge += item.year;
+    //       teams[index].member.push(item);
+    //     });
+    //   };
+    //   reader.readAsText(file, this);
+    // }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
